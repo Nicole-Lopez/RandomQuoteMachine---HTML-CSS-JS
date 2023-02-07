8e911f2a2e38318b8aff2b23f3c10b5caa9132cf
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-02-07 04:07:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const quote = document.getElementById('text')
+const author = document.getElementById('author')
+const tweetButton = document.getElementById('tweet-quote')
+const nextQuote = document.getElementById('new-quote')
+
+const getQuotes = async () =>{
+	await axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+		.then(res => renderData(res.data))
+		.catch(err => console.log(err))
+}
+window.onload = getQuotes()
+
+
+const renderData = (info) => {
+	let randomQuote = info.quotes[Math.floor(Math.random()*info.quotes.length)]
+	
+	quote.textContent = randomQuote.quote;
+	author.textContent = randomQuote.author
+	tweetButton.href = `https://twitter.com/intent/tweet?text=${randomQuote.quote} -${randomQuote.author}`
+}
+
+
+
+nextQuote.addEventListener("click", () => {
+	getQuotes()
+})
+
