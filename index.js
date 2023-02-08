6d7e1c5fc85577a8a3f57211bf5c1b68d10d3012
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-02-08 00:27:06 -0300

    new design 3D

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const author = document.getElementById('author')
 const tweetButton = document.getElementById('tweet-quote')
 const nextQuote = document.getElementById('new-quote')
 
+
 const getQuotes = async () =>{
 	await axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
 		.then(res => renderData(res.data))
@@ -14,6 +15,16 @@ window.onload = getQuotes()
 const renderData = (info) => {
 	let randomQuote = info.quotes[Math.floor(Math.random()*info.quotes.length)]
 	
+	// ---- TEXT ANIMATION ----
+	quote.classList.remove("run-animation");
+	void quote.offsetWidth;
+	quote.classList.add("run-animation");
+
+	author.classList.remove("run-animation");
+	void author.offsetWidth;
+	author.classList.add("run-animation");
+	//------------------------ 
+
 	quote.textContent = randomQuote.quote;
 	author.textContent = randomQuote.author
 	tweetButton.href = `https://twitter.com/intent/tweet?text=${randomQuote.quote} -${randomQuote.author}`
