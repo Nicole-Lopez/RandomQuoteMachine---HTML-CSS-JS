e0ae740427c94e5409c9e6860a4ef044fe2b7032
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-02-08 21:03:31 -0300

    design finish

--- index.js
+++ index.js
@@ -2,37 +2,43 @@ const quote = document.getElementById('text')
 const author = document.getElementById('author')
 const tweetButton = document.getElementById('tweet-quote')
 const nextQuote = document.getElementById('new-quote')
-
+const toggleButtons = document.getElementById('menu')
 
 const getQuotes = async () =>{
 	await axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
 		.then(res => renderData(res.data))
 		.catch(err => console.log(err))
 }
-window.onload = getQuotes()
 
+window.onload = getQuotes();
 
-const renderData = (info) => {
-	let randomQuote = info.quotes[Math.floor(Math.random()*info.quotes.length)]
-	
-	// ---- TEXT ANIMATION ----
-	quote.classList.remove("run-animation");
-	void quote.offsetWidth;
-	quote.classList.add("run-animation");
-
-	author.classList.remove("run-animation");
-	void author.offsetWidth;
-	author.classList.add("run-animation");
-	//------------------------ 
-
-	quote.textContent = randomQuote.quote;
-	author.textContent = randomQuote.author
-	tweetButton.href = `https://twitter.com/intent/tweet?text=${randomQuote.quote} -${randomQuote.author}`
+const fadeInAnimation = (e) => {
+	e.classList.remove("run-animation");
+	void e.offsetWidth;
+	e.classList.add("run-animation");	
 }
 
+const renderData = (info) => {
+	let randomQuote = info.quotes[Math.floor(Math.random()*info.quotes.length)];
+	
+	fadeInAnimation(quote);
+	fadeInAnimation(author);
+
+	quote.textContent = randomQuote.quote;
+	author.textContent = randomQuote.author;
+	tweetButton.href = `https://twitter.com/intent/tweet?text=${randomQuote.quote} -${randomQuote.author}`;
+}
+
+nextQuote.addEventListener("click", () => getQuotes())
 
 
-nextQuote.addEventListener("click", () => {
-	getQuotes()
+toggleButtons.addEventListener("click", () => {
+	if (nextQuote.classList[0] === 'visible') {
+		nextQuote.classList.remove("visible");	
+		tweetButton.classList.remove("visible");		
+	} else {
+		nextQuote.classList.add("visible");	
+		tweetButton.classList.add("visible");	
+	}
 })
 
